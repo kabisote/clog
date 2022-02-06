d5b7bc6cb8b3ed60772fb0fc6b2197e47719b8d3
Marc Linsangan <kabisote@gmail.com>
2022-02-06 15:26:23 +0800

    Fixed territory phone numbers presentation.

--- Web/View/Territories/Show.hs
+++ Web/View/Territories/Show.hs
@@ -1,18 +1,35 @@
 module Web.View.Territories.Show where
 import Web.View.Prelude
+import Web.View.PhoneNumbers.Index (renderPhoneNumber)
 
-data ShowView = ShowView { territory :: Territory }
+newtype ShowView = ShowView { territory :: Include "phoneNumbers" Territory }
 
 instance View ShowView where
     html ShowView { .. } = [hsx|
-        {breadcrumb}
-        <h1>{ get #name territory }</h1>
-        <p>{territory}</p>
-        <div><a href={NewPhoneNumberAction (get #id territory)} class="btn btn-primary">Add Phone Number</a></div>
+        <h1>{ get #name territory } (Group { get #groupNumber territory })</h1>
+        <div><a href={ NewPhoneNumberAction (get #id territory) } class="btn btn-primary my-4">Add Phone Number</a></div>
+        <div>{ forEach (get #phoneNumbers territory) renderPhoneNumber }</div>
 
     |]
         where
             breadcrumb = renderBreadcrumb
-                            [ breadcrumbLink "Territories" TerritoriesAction
+                            [ breadcrumbLink "Back" TerritoriesAction
                             , breadcrumbText "Show Territory"
                             ]
+            renderPhoneNumber phoneNumber = [hsx|
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Phone Numbers</th>
+                                <th scope="col">Source</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class={ if (get #doNotCall phoneNumber) then "table-danger" :: Text else "table-default" }>
+                                <td><a href={ ShowPhoneNumberAction (get #id phoneNumber)}>0{ get #phoneNumber phoneNumber }</a></td>
+                                <td>{ get #source phoneNumber }</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                |]
+
