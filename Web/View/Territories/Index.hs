d3424ab0f5e1d149d523deed1ffe6e70e4230cf8
Marc Linsangan <kabisote@gmail.com>
2022-02-07 13:45:23 +0800

    Grouped territories by group number. Changed presentation from table to cards.

--- Web/View/Territories/Index.hs
+++ Web/View/Territories/Index.hs
@@ -1,43 +1,124 @@
 module Web.View.Territories.Index where
 import Web.View.Prelude
 
-data IndexView = IndexView { territories :: [Territory]  }
+newtype IndexView = IndexView { territories :: [Territory]  }
 
 instance View IndexView where
     html IndexView { .. } = [hsx|
-        {breadcrumb}
 
-        <h1>Territories <a href={pathTo NewTerritoryAction} class="btn btn-primary ml-4">+ New</a></h1>
-        <div class="table-responsive">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Territory</th>
-                        <th>Group</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>{forEach territories renderTerritory}</tbody>
-            </table>
-            
+        <h1 class="mb-4 pt-4">Territories <a href={pathTo NewTerritoryAction} class="btn btn-primary ml-4">+ New</a></h1>
+
+        <div class="row  row-cols-1 row-cols-md-3 g-4">
+            <!-- Common Territory -->
+            <div class="col">
+                <div class="card my-4">
+                    <h5 class="card-header">Common Territory</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 0)}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col">
+                <!-- Group 1 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 1</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 1)}
+                    </ul>
+                </div>
+            </div>
+                
+            <div class="col">
+                <!-- Group 2 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 2</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 2)}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col">
+                <!-- Group 3 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 3</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 3)}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col">
+                <!-- Group 4 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 4</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 4)}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col">
+                <!-- Group 5 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 5</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 5)}
+                    </ul>
+                </div>
+            </div>
+                
+            <div class="col">
+                <!-- Group 6 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 6</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 6)}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col">
+                <!-- Group 7 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 7</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 7)}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col">
+                <!-- Group 8 -->
+                <div class="card my-4">
+                    <h5 class="card-header">Group 8</h5>
+                    <ul class="list-group list-group-flush">
+                        {forEach territories (renderTerritory 8)}
+                    </ul>
+                </div>
+            </div>
         </div>
+
     |]
         where
             breadcrumb = renderBreadcrumb
                 [ breadcrumbLink "Territories" TerritoriesAction
                 ]
 
-renderTerritory :: Territory -> Html
-renderTerritory territory = [hsx|
-    <tr>
-        <td>{get #name territory}</td>
-        <td>{get #groupNumber territory}</td>
-        <td>
-            <a href={ShowTerritoryAction (get #id territory)}>Show</a>
-            |
-            <a href={EditTerritoryAction (get #id territory)} class="text-muted">Edit</a>
-            |
-            <a href={DeleteTerritoryAction (get #id territory)} class="js-delete text-muted">Delete</a>
-        </td>
-    </tr>
+renderTerritory :: Int -> Territory -> Html
+renderTerritory n territory = [hsx|
+    {when (n == get #groupNumber territory) territoryDetails}
 |]
+    where
+        territoryDetails = [hsx|
+            <li class="list-group-item">
+                <a href={ShowTerritoryAction (get #id territory)}>{get #name territory}</a>
+                (
+                <a href={EditTerritoryAction (get #id territory)} class="text-muted">Edit</a>
+                |
+                <a href={DeleteTerritoryAction (get #id territory)} class="js-delete text-muted">Delete</a>
+                )
+            </li>
+        |]
+
