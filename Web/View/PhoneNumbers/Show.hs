11be610cfcc3579dc25113823b15fbf87da726de
Marc Linsangan <kabisote@gmail.com>
2022-02-15 11:22:40 +0800

    Only show Delete phone number button to admins. Add edit and delete buttons to calls.

--- Web/View/PhoneNumbers/Show.hs
+++ Web/View/PhoneNumbers/Show.hs
@@ -17,7 +17,7 @@ instance View ShowView where
         <p>
             <a class="btn btn-secondary mr-2" href={ ShowTerritoryAction (get #territoryId phoneNumber) }>Back to Territory</a>
             <a class="btn btn-primary mr-2" href={EditPhoneNumberAction (get #id phoneNumber)}>Edit</a>
-            <a class="btn btn-danger mr-2 js-delete" href={DeletePhoneNumberAction (get #id phoneNumber)}>Delete</a>
+            { when isAdmin renderDeletePhoneNumberButton }
         </p>
         <hr />
         <h3 class="my-4">Records</h3>
@@ -31,19 +31,33 @@ instance View ShowView where
                             , breadcrumbText "Show PhoneNumber"
                             ]
 
+            isAdmin = get #userRole currentUser == 1
+
+            renderDeletePhoneNumberButton = [hsx|
+                    <a class="btn btn-danger js-delete" href={DeletePhoneNumberAction (get #id phoneNumber)}>Delete</a>
+                |]
+
+
 renderCall :: Call -> Html
 renderCall call = [hsx|
-    <dl class="row shadow-sm p-3 mb-5 bg-body rounded">
-        <dt class="col-sm-2">Publisher(s)</dt>
-        <dd class="col-sm-10">{ get #agents call }</dd>
+        <dl class="row shadow-sm p-3 mb-5 bg-body rounded">
+            <dt class="col-sm-2">Publisher(s)</dt>
+            <dd class="col-sm-10">{ get #agents call }</dd>
 
-        <dt class="col-sm-2">When Called</dt>
-        <dd class="col-sm-10">{ get #createdAt call |> timeAgo }</dd>
+            <dt class="col-sm-2">When Called</dt>
+            <dd class="col-sm-10">{ get #createdAt call |> timeAgo }</dd>
 
-        <dt class="col-sm-2">Remarks</dt>
-        <dd class="col-sm-10">{ get #remarks call |> renderMarkdown }</dd>
-    </dl>
-|]
+            <dt class="col-sm-2">Remarks</dt>
+            <dd class="col-sm-10">{ get #remarks call |> renderMarkdown }</dd>
+
+            <dt class="col-sm-2"></dt>
+            <dd class="col-sm-10">
+                <a class="btn btn-dark btn-sm mr-2" href={ EditCallAction (get #id call) }>Edit</a>
+                <a class="btn btn-danger btn-sm js-delete" href={ DeleteCallAction (get #id call) }>Delete</a>
+            </dd>
+        </dl>
+    |]
+
 
 renderMarkdown :: Text -> Html
 renderMarkdown text =
